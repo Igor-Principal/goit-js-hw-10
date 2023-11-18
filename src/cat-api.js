537ad44e0f232dc5100222cc63e6485bdae7db19
Igor-Principal <igorprincipal@gmail.com>
2023-11-18 20:58:41 +0200

    Recycled connection

--- src/cat-api.js
+++ src/cat-api.js
@@ -4,25 +4,13 @@ const API_KEY =
   'live_qRQinlwL0qToGk7sIwBtUVXCniSQdcvy2d8vwPSUJlRdswOF3C6LNhK5RhjtMWd4';
 
 function fetchBreeds() {
-  const END_POINT = 'breeds';
-
-  const params = {
-    api_key: API_KEY,
-  };
   return axios
-    .get(`${BASE_URL}/${END_POINT}?${params}`)
+    .get(`${BASE_URL}/breeds?api_key=${API_KEY}`)
     .then(({ data }) => data);
 }
 function fetchCatByBreed(breedId) {
-  const END_POINT = 'images/search';
-
-  const catParams = {
-    api_key: API_KEY,
-    breed_ids: breedId,
-  };
-
   return axios
-    .get(`${BASE_URL}/${END_POINT}`, { params: catParams })
+    .get(`${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`)
     .then(({ data }) => data);
 }
 export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -21,9 +21,8 @@ function selectBreed(evt) {
   const selectedOptionId = evt.target.value;
   fetchCatByBreed(selectedOptionId)
     .then(data => {
-      refs.infoContainer.innerHTML = `<div class="box-img">
-      <img src="${data.url}" alt="" />
-      </div>`;
+      const { url, breeds } = data[0];
+      refs.infoContainer.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
     })
     .catch(err => {
       console.log(err);
